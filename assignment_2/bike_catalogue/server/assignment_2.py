11eba175dd6d1b5ba9826be2fe998ba0635f596c
azaat <a.r.akhmetyanov@gmail.com>
2020-03-29 16:04:13 +0300

    Suggested changes

--- assignment_2/bike_catalogue/server/assignment_2.py
+++ assignment_2/bike_catalogue/server/assignment_2.py
@@ -1,7 +1,7 @@
 import sqlite3
-from bs4 import BeautifulSoup
-from flask import Flask, g, request, render_template
+from flask import Flask, g
 from flask_cors import CORS
+from bs4 import BeautifulSoup
 import requests
 import json
 
@@ -10,26 +10,28 @@ CORS(app)
 
 DATABASE = 'bike_database.sqlite'
 
+
 def parse_velostrana(db_conn):
     base_url = 'https://www.velostrana.ru'
-    response = requests.get('https://www.velostrana.ru/shosseynye-velosipedy/shosseynye/')   
+    response = requests.get('https://www.velostrana.ru/shosseynye-velosipedy/shosseynye/')
     html_doc = response.content
     soup = BeautifulSoup(html_doc, 'html.parser')
     products = soup.find_all('div', class_='product__in')
 
     for product in products:
-        model = product.find('span', class_ ='product__model').contents[0]
+        model = product.find('span', class_='product__model').contents[0]
         price = product.find('p', class_='product__price-new').contents[0]
         img_link = product.find('img', class_='product__img-image')['src']
         add_entry(db_conn, model, price, base_url + img_link)
 
+
 def parse_velopiter(db_conn):
     base_url = 'https://www.velopiter.ru'
-    response = requests.get('https://www.velopiter.ru/fast/nedorogie-gorodskie-velosipedy/1.htm')   
+    response = requests.get('https://www.velopiter.ru/fast/nedorogie-gorodskie-velosipedy/1.htm')
     html_doc = response.content
     soup = BeautifulSoup(html_doc, 'html.parser')
     products = soup.find_all('div', class_='proditem1')
-    
+
     for product in products:
         model = product.find('div', class_='proditemtitle').find('a')['title']
         price = product.find('div', class_='price').find('span').contents[0]
@@ -43,6 +45,7 @@ def get_db():
         db_conn = g._database = sqlite3.connect(DATABASE)
     return db_conn
 
+
 def add_entry(conn, model, price, img_link):
     cursor = conn.cursor()
     cursor.execute(
@@ -52,13 +55,16 @@ def add_entry(conn, model, price, img_link):
     )
     conn.commit()
 
+
 @app.route('/')
 def get_all():
     db_cursor = get_db().cursor()
     db_cursor.row_factory = sqlite3.Row
     db_cursor.execute('SELECT * From Bikes ORDER BY price ASC')
     bikes = db_cursor.fetchall()
-    return render_template('index.html', bikes = bikes)
+    json_result = json.dumps([dict(row) for row in bikes])
+    return json_result
+
 
 def init_db():
     with app.app_context():
@@ -70,17 +76,20 @@ def init_db():
         )
         db_conn.commit()
 
+
 def get_info():
     with app.app_context():
         conn = get_db()
         parse_velostrana(conn)
         parse_velopiter(conn)
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db_conn = getattr(g, '_database', None)
     if db_conn is not None:
         db_conn.close()
+
 
 if __name__ == '__main__':
     init_db()
