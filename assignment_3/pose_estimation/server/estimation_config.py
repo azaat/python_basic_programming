85fbddded196df4e037b63d841e7dcc46ce889a2
azaat <a.r.akhmetyanov@gmail.com>
2020-04-14 12:01:52 +0300

    Code refactor

--- assignment_3/pose_estimation/server/estimation_config.py
+++ assignment_3/pose_estimation/server/estimation_config.py
@@ -1,19 +1,19 @@
 # Model files
-protoFile = "../mpi/pose_deploy_linevec_faster_4_stages.prototxt"
-weightsFile = "../mpi/pose_iter_160000.caffemodel"
+proto_file = "../mpi/pose_deploy_linevec_faster_4_stages.prototxt"
+weights_file = "../mpi/pose_iter_160000.caffemodel"
 
 # Resize input to the following width and height
-inWidth = 400
-inHeight = 400
+in_width = 400
+in_height = 400
 
 # Estimation threshold
 threshold = 0.1
 
 # Point configuration
-nPoints = 15
-posePairs = [
-    [0, 1],   [1, 2], [2, 3],  [3, 4], 
-    [1, 5],   [5, 6], [6, 7],  [1, 14], 
-    [14, 8],  [8, 9], [9, 10], [14, 11], 
+n_points = 15
+pose_pairs = [
+    [0, 1],   [1, 2], [2, 3],  [3, 4],
+    [1, 5],   [5, 6], [6, 7],  [1, 14],
+    [14, 8],  [8, 9], [9, 10], [14, 11],
     [11, 12], [12, 13]
 ]
--- assignment_3/pose_estimation/server/pose_estimation.py
+++ assignment_3/pose_estimation/server/pose_estimation.py
@@ -8,11 +8,11 @@ def get_points(output, frame):
     W = output.shape[3]
 
     points = []
-    for i in range(cfg.nPoints):
+    for i in range(cfg.n_points):
         # Body parts confidence map
-        probMap = output[0, i, :, :]
+        prob_map = output[0, i, :, :]
 
-        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+        min_val, prob, min_loc, point = cv2.minMaxLoc(prob_map)
 
         x = (frame.shape[1] * point[0]) / W
         y = (frame.shape[0] * point[1]) / H
@@ -26,7 +26,7 @@ def get_points(output, frame):
 
 def draw_points(img_path):
     # Read the network into Memory
-    net = cv2.dnn.readNetFromCaffe(cfg.protoFile, cfg.weightsFile)
+    net = cv2.dnn.readNetFromCaffe(cfg.proto_file, cfg.weights_file)
 
     # Read image
     frame = cv2.imread(img_path)
@@ -34,18 +34,18 @@ def draw_points(img_path):
         raise ValueError('Input image is corrupt')
 
     # Prepare the frame to be fed to the network
-    inpBlob = cv2.dnn.blobFromImage(
+    inp_blob = cv2.dnn.blobFromImage(
         frame, 1.0 / 255,
-        (cfg.inWidth, cfg.inHeight),
+        (cfg.in_width, cfg.in_height),
         (0, 0, 0), swapRB=False, crop=False
     )
 
-    net.setInput(inpBlob)
+    net.setInput(inp_blob)
     output = net.forward()
     points = get_points(output, frame)
 
     # Draw points
-    for pair in cfg.posePairs:
+    for pair in cfg.pose_pairs:
         partA = pair[0]
         partB = pair[1]
 
